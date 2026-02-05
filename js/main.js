967978a016614a07f13c74cb71146fe67d257679
Kirill-eng333 <bushtakov@internet.ru>
2026-02-05 19:50:52 +0500

    init: add home-site project

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function () {
+  $('.banner-section__slider').slick({
+    dots: true,
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/content/rith (12).png" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/content/rith (12).png" alt=""></button>'
+  });
+
+  $('.tab').on('click', function(e){
+    e.preventDefault();
+
+    $($(this).siblings()).removeClass('tab--active');
+    $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
+
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tabs-content--active');
+  });
+
+  $('.product-item__favorite').on('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $(this).toggleClass('product-item__favorite--active');
+  });
+
+
+  $('.product-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/content/rith (12).png" alt=""></button>',
+    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/content/rith (12).png" alt=""></button>'
+  })
+  
+
+});
